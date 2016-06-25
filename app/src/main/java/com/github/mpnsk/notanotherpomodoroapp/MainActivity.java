cc3758651d0c171559a6b1ed779f5112d6e14257
mpnsk <mpnsk@users.noreply.github.com>
2016-06-25 19:37:13 +0200

    Create an alarm to start a Dummy activity in x seconds, which vibrates.

--- app/src/main/java/com/github/mpnsk/notanotherpomodoroapp/MainActivity.java
+++ app/src/main/java/com/github/mpnsk/notanotherpomodoroapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.github.mpnsk.notanotherpomodoroapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.button_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupAlarm();
+            }
+        });
+    }
+
+    private void setupAlarm() {
+        Intent intent = new Intent(this, DummyActivity.class);
+        PendingIntent alarmIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 5 * 1000,
+                alarmIntent);
     }
 
 }
